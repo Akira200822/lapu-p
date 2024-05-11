9422f1a3482a84e51885c1ebcb1fa888336f0e38
Akira200822 <rachitelem@yandex.ru>
2024-05-11 17:44:12 +0400

    create base window

--- lapur bear.py
+++ lapur bear.py
@@ -1,2 +1,14 @@
 import arcade
 
+SCREEN_WIDTH=800
+SCREEN_HEIGHT=600
+SCREEN_TITLE="Flappy Bird"
+
+class Game(arcade.Window):
+    def __init__(self,width,height,title):
+        super().__init__(width, height, title)
+
+
+
+window=Game(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE)
+arcade.run()
