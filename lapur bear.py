dd9e2b473fd8e056c73ace1df0878a69e991739b
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-05-11 15:50:11 +0200

    update main pyhton file

--- lapur bear.py
+++ lapur bear.py
@@ -4,10 +4,18 @@ SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
 SCREEN_TITLE="Flappy Bird"
 
+class Animation(arcade.Sprite):
+    pass
+
 class Game(arcade.Window):
     def __init__(self,width,height,title):
         super().__init__(width, height, title)
 
+    def on_draw(self):
+        pass
+
+    def update(self, delta_time):
+	pass
 
 
 window=Game(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE)
